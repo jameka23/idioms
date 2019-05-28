3850c8ba8753a6d0074fa3ca7dec3debaee1ac4b
Jameka Echols <jameka.echols@gmail.com>
2019-05-28 13:46:30 -0500

    first commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace idioms
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
